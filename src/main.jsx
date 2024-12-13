87c215e8ac94a34e4436a3ea9f891363a959c725
gahyunggim <gh.kim@enb-solution.com>
2024-12-13 18:18:54 +0900

    msw 설정 변경

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,18 @@ import { LocationProvider } from "./contexts/LocationProvider.jsx";
 
 // 서비스 워커 초기화
 // if (import.meta.env.DEV) {
-const { worker } = await import("./mocks/browser.js");
-await worker.start({
-  onUnhandledRequest: "warn",
-});
+//   const { worker } = await import("./mocks/browser.js");
+//   await worker.start({
+//     onUnhandledRequest: "warn",
+//   });
 // }
+
+if (import.meta.env.VITE_ENABLE_MOCKS === "true") {
+  const { worker } = await import("./mocks/browser.js");
+  await worker.start({
+    onUnhandledRequest: "warn",
+  });
+}
 
 // React 애플리케이션 렌더링
 createRoot(document.getElementById("root")).render(
